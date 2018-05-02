a6cfc00a02bb3f93a8a8fac2ff9a3f1109b61628
Wiley <33207361+Wileysec@users.noreply.github.com>
2018-05-02 17:01:07 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-$(function(){
-  $(document).ready(function(){
-      function PageHeight(){
-          ch=$('.content').outerHeight();
-          sh=$('.sidebar').outerHeight();
-          if(ch>sh){
-              $('.sidebar').css({'height':ch+'px'});
-          }
-          if(sh>ch){
-              $('.content').css({'height':sh+'px'});
-          }
-      }
+$(document).ready(function(){
+    function PageHeight(){
+        ch=$('.content').outerHeight();
+        sh=$('.sidebar').outerHeight();
+        if(ch>sh){
+            $('.sidebar').css({'height':ch+'px'});
+        }
+        if(sh>ch){
+            $('.content').css({'height':sh+'px'});
+        }
+    }
 
-      PageHeight();
+    PageHeight();
 
-      $(window).resize(function(){
-          PageHeight();
-      });
-      $('#searchModal').on('shown.bs.modal',function(e){
-          $('#search').focus(); //通过ID找到对应输入框，让其获得焦点
-      });
-  });
+    $(window).resize(function(){
+        PageHeight();
+    });
+    $('#searchModal').on('shown.bs.modal',function(e){
+        $('#search').focus(); //通过ID找到对应输入框，让其获得焦点
+    });
 });
