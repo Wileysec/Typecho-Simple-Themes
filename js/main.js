280ab5b42df2d5542149d5f2470ac811eed28312
Wiley <33207361+Wileysec@users.noreply.github.com>
2018-05-18 20:27:38 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
     function PageHeight(){
+        
+        // 获取内容框的高度
         ch=$('.content').outerHeight();
+        
+        // 获取侧边栏的高度
         sh=$('.sidebar').outerHeight();
+        
+        // 如果内容框高度大于侧边栏高度
         if(ch>sh){
             $('.sidebar').css({'height':ch+'px'});
         }
@@ -11,11 +17,21 @@ $(document).ready(function(){
     }
 
     PageHeight();
-
+    
+    // 当页面尺寸调整时触发PageHeight事件
     $(window).resize(function(){
         PageHeight();
     });
     $('#searchModal').on('shown.bs.modal',function(e){
         $('#search').focus(); //通过ID找到对应输入框，让其获得焦点
+    });
+    
+    // 当滚动到80px时，隐藏导航条。回到顶部，显示导航条
+    $(document).scroll(function(){
+        if($(document).scrollTop()>80){
+            $('.navbar').fadeOut(500);
+        }else{
+            $('.navbar').fadeIn(500);
+        }
     });
 });
