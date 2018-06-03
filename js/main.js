e9a28075ef6bc1fd07040fac9fb6ed1d8cbce132
Wiley <33207361+Wileysec@users.noreply.github.com>
2018-06-03 15:21:05 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,12 +26,4 @@ $(document).ready(function(){
         $('#search').focus(); //通过ID找到对应输入框，让其获得焦点
     });
     
-    // 当滚动到80px时，隐藏导航条。回到顶部，显示导航条
-    $(document).scroll(function(){
-        if($(document).scrollTop()>80){
-            $('.navbar').fadeOut(500);
-        }else{
-            $('.navbar').fadeIn(500);
-        }
-    });
 });
